7b58c79a4c6414bd54a9707d12a1950400ae3c13
Nidhi Kejriwal <kejriwal.nidhi23@gmail.com>
2023-05-19 20:04:43 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 '''
 Defining a class to declare the path for multiple purposes (train_data, test_data, or any output)
 Using @dataclass detractor allows us to define our set of variables in the class, otherwise we have to use init and initialize the variables
@@ -53,4 +55,8 @@ On the other hand, if your module is imported in another module then the string 
 '''
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    data_transformation.initiate_data_transformation(train_path, test_path)
